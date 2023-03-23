0c3b3044c5f7ccd47edfbd740ea342193b72b74e
Etienne Pretorius <ettienne_pretorius@yahoo.co.uk>
2023-03-23 00:28:57 +0000

    - Corrected includes before <cmocka.h> - Update test dependency ``aquarium-cmocka`` to 1.5.2 - Update dependency ``aquarium-sea-urchin`` to 1.5.1 - Update dependency ``aquarium-sea-turtle`` to 4.0.1 - Update dependency ``aquarium-triggerfish`` to 2.0.1

--- cmake/FetchAquariumSeaTurtle.cmake
+++ cmake/FetchAquariumSeaTurtle.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
         aquarium-sea-turtle
         GIT_REPOSITORY https://github.com/pretore/aquarium-sea-turtle.git
-        GIT_TAG v3.1.0
+        GIT_TAG v4.0.1
         GIT_SHALLOW 1
 )
 
